3fa92c18c7413659dd95caf75f3ab8afcbf7e5c5
8989as <ahmeedsaeed7@gmail.com>
2025-07-23 15:28:06 +0300

    About

--- resources/js/pages/About.jsx
+++ resources/js/pages/About.jsx
@@ -1,10 +1,17 @@
 import About from "../components/About/About";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
+import { useTranslation } from "react-i18next";
 const AboutUs = () => {
+    const { t } = useTranslation();
     return (
         <>
             <Navbar />
+            <Breadcrumb items={[
+                { label: t('home'), url: '/' },
+                { label: t('about'), active: true }
+            ]} />
             <div className="container">
                 <About />
             </div>
